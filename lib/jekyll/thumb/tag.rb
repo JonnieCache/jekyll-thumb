86cd1578dd861df2c6fb239b87d08cbd1104a0d9
jonnie <jonnie@cleverna.me>
2019-05-29 17:30:34 +0100

    dont pollute img tag with link attribute

--- lib/jekyll/thumb/tag.rb
+++ lib/jekyll/thumb/tag.rb
@@ -23,8 +23,9 @@ module Jekyll
       site = context.registers[:site]
       img_attrs = generate_image(site, options["src"], options)
 
+      link = options.delete 'link'
       img = %Q{<img #{options.merge(img_attrs).map {|k,v| "#{k}=\"#{v}\""}.join(" ")}>}
-      return img if options['link'] == false
+      return img if link == false
       
       %Q{<a href="#{options['src']}" target="_blank" class="thumb">#{img}</a>}
     end
